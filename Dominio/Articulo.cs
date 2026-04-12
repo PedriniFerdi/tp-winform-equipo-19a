cb39f9d0410ffb2df5284fce4248ba7d7055505e
unknown <pedritrades97@gmail.com>
2026-04-12 19:04:06 -0300

    Creacion del proyecto y primera clase

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,19 @@
+﻿namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+        public Articulo()
+        {
+            Imagenes = new List<Imagen>();
+        }
+
+    }
+}
